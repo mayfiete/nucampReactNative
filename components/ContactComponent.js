9f747523709104114ab1e06921e1d9853d3a43d6
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-12-18 07:59:21 -0500

    workshop 2 task 2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -4,6 +4,8 @@ import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { ScrollView } from 'react-native';
 import { Text } from 'react-native-elements';
+import { Card } from 'react-native-elements';
+import { View } from 'react-native';
 
 class Contact extends Component {
     static navigationOptions = {
@@ -14,8 +16,33 @@ class Contact extends Component {
         const { navigate } = this.props.navigation;
         return (
             <ScrollView>
+                <Card wrapperStyle={{ margin: 20 }} title='Contact Information'>
+                    <View
+                        style={{
+                            borderBottomColor: 'lightgray',
+                            borderBottomWidth: 1,
+                            marginBottom: 10
+                        }}
+                    />
+                    <Text style={{ marginTop: 10 }}>
+                        1 Nucamp Way
+                    </Text>
+                    <Text>
+                        Seattle, WA 98001
+                    </Text>
+                    <Text style={{ marginBottom: 10 }}>
+                        U.S.A.
+                    </Text>
+                    <Text>
+                        Phone: 1-206-555-1234
+                    </Text>
+                    <Text>
+                        Email: campsites@nucamp.co
+                    </Text>
 
-            </ScrollView>
+                </Card>
+
+            </ScrollView >
         );
     }
 }
