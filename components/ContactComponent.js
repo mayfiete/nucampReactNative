de5c9652b899437e45e0139662ea100fa47fea65
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2022-01-01 17:16:49 -0500

    exercise - Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -6,6 +6,7 @@ import { ScrollView } from 'react-native';
 import { Text } from 'react-native-elements';
 import { Card } from 'react-native-elements';
 import { View } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     static navigationOptions = {
@@ -16,32 +17,33 @@ class Contact extends Component {
         const { navigate } = this.props.navigation;
         return (
             <ScrollView>
-                <Card wrapperStyle={{ margin: 20 }} title='Contact Information'>
-                    <View
-                        style={{
-                            borderBottomColor: 'lightgray',
-                            borderBottomWidth: 1,
-                            marginBottom: 10
-                        }}
-                    />
-                    <Text style={{ marginTop: 10 }}>
-                        1 Nucamp Way
-                    </Text>
-                    <Text>
-                        Seattle, WA 98001
-                    </Text>
-                    <Text style={{ marginBottom: 10 }}>
-                        U.S.A.
-                    </Text>
-                    <Text>
-                        Phone: 1-206-555-1234
-                    </Text>
-                    <Text>
-                        Email: campsites@nucamp.co
-                    </Text>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card wrapperStyle={{ margin: 20 }} title='Contact Information'>
+                        <View
+                            style={{
+                                borderBottomColor: 'lightgray',
+                                borderBottomWidth: 1,
+                                marginBottom: 10
+                            }}
+                        />
+                        <Text style={{ marginTop: 10 }}>
+                            1 Nucamp Way
+                        </Text>
+                        <Text>
+                            Seattle, WA 98001
+                        </Text>
+                        <Text style={{ marginBottom: 10 }}>
+                            U.S.A.
+                        </Text>
+                        <Text>
+                            Phone: 1-206-555-1234
+                        </Text>
+                        <Text>
+                            Email: campsites@nucamp.co
+                        </Text>
 
-                </Card>
-
+                    </Card>
+                </Animatable.View>
             </ScrollView >
         );
     }
