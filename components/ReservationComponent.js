40acb392223c59a858559f8e777f57ec21040c07
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2022-01-05 07:06:03 -0500

    exercise - local notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -40,8 +41,10 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () =>
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
                         this.resetForm()
+                    }
                 }
             ],
             { cancelable: false }
@@ -56,6 +59,33 @@ class Reservation extends Component {
             showCalendar: false,
         });
     }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
 
 
 
